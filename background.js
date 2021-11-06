6fc3e16bb5877fb9031f7bb5a8f0e09c0e79e3ee
clairefro <claire.froelich@gmail.com>
2021-11-05 19:34:53 -0700

    Add install scripts

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ censor: true }, function () {
+    console.log("Postman sensitive variable sensorship enabled");
+  });
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: "www.postman.com" },
+        }),
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()],
+    },
+  ]);
+});
